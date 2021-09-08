1c607e264d8d0bfe5e608d8eedd15cb5d2b648ae
Sam Pomerantz <sampom100@gmail.com>
2021-09-08 12:52:12 -0400

    merge graphs

--- yMethods.py
+++ yMethods.py
@@ -13,33 +13,28 @@ for date in optionChain:
 
 #get top 10 volume options for single date
 print(optionChain)
-optionStrikes = ticker.option_chain(optionChain[2])
-calls = optionStrikes.calls.nlargest(10, 'volume')
-calls = calls.drop(["volume","lastTradeDate","strike","lastPrice","bid","ask","openInterest","impliedVolatility","inTheMoney","contractSize","currency","change","percentChange"],axis=1)
+for x in range(len(optionChain)):
+    optionStrikes = ticker.option_chain(optionChain[x])
+    calls = optionStrikes.calls.nlargest(10, 'volume')
+    calls = calls.drop(["volume","lastTradeDate","strike","lastPrice","bid","ask","openInterest","impliedVolatility","inTheMoney","contractSize","currency","change","percentChange"],axis=1)
 
-#column of DataFrame into list
-callsList = []
-for x in calls.values.tolist():
-    callsList.append(x[0])
+    #column of DataFrame into list
+    callsList = []
+    for x in calls.values.tolist():
+        callsList.append(x[0])
 
-def singleData(optionID):
-    data = yf.download([optionID])
-    data = data.drop(["Open", "High", "Low", "Close", "Adj Close"], axis=1)
-    data.plot()
+    def singleData(optionID):
+        data = yf.download([optionID])
+        data = data.drop(["Open", "High", "Low", "Close", "Adj Close"], axis=1)
+        return data
+
+    frame = singleData(callsList[0])
+
+    for x in range(len(callsList)-4):
+        tmpFrame = singleData(callsList[x+1])
+        tmpFrame.rename(columns={"Volume":str(callsList[x])}, inplace=True)
+        frame = pd.merge(frame,tmpFrame, on="Date")
+
+    print(frame)
+    frame.plot()
     plt.show()
-    return data
-
-#get single option data
-
-#for id in callsList:
-    temp = singleData(id)
-    print(temp)
-    
-
-d1 = (singleData(callsList[0]))
-d2 = (singleData(callsList[1]))
-
-result = d1.append(d2)
-result.plot()
-print(result)
-plt.show()
