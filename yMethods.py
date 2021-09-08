6c90f200ff3d7547215be2e12c6eda463123b683
Sam Pomerantz <sampom100@gmail.com>
2021-09-08 12:26:31 -0400

    Update yMethods.py

--- yMethods.py
+++ yMethods.py
@@ -1,3 +1,4 @@
+from numpy import single
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -11,7 +12,8 @@ for date in optionChain:
     pass
 
 #get top 10 volume options for single date
-optionStrikes = ticker.option_chain(optionChain[10])
+print(optionChain)
+optionStrikes = ticker.option_chain(optionChain[2])
 calls = optionStrikes.calls.nlargest(10, 'volume')
 calls = calls.drop(["volume","lastTradeDate","strike","lastPrice","bid","ask","openInterest","impliedVolatility","inTheMoney","contractSize","currency","change","percentChange"],axis=1)
 
@@ -23,13 +25,21 @@ for x in calls.values.tolist():
 def singleData(optionID):
     data = yf.download([optionID])
     data = data.drop(["Open", "High", "Low", "Close", "Adj Close"], axis=1)
+    data.plot()
+    plt.show()
     return data
 
 #get single option data
-for id in callsList:
+
+#for id in callsList:
     temp = singleData(id)
-    temp.plot()
-    plt.show()
-    plt.clf()
+    print(temp)
+    
 
+d1 = (singleData(callsList[0]))
+d2 = (singleData(callsList[1]))
 
+result = d1.append(d2)
+result.plot()
+print(result)
+plt.show()
